157814a20a16aecfaf6d9bc96820a3140145dd48
Vladimir Kulikov <vladimir_kulikov@epam.com>
2021-04-20 11:28:35 +0300

    create product component

---
+++ src/app/products/components/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CartService } from 'src/app/cart/services/cart.service';
+import { ProductModel } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  @Input() product!: ProductModel;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+  }
+
+  onBuy(product: ProductModel): void {
+    console.log(`"${product.name}" have been added to the cart`);
+    this.cartService.addProduct(product)
+  }
+}
