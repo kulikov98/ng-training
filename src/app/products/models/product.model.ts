ab60a8f63d853f354f26f0470eb72efffe6babff
Vladimir Kulikov <vladimir_kulikov@epam.com>
2021-04-20 11:29:48 +0300

    create product model

---
+++ src/app/products/models/product.model.ts
@@ -0,0 +1,11 @@
+import { Category } from "../enums/category.enum";
+
+export class ProductModel {
+    constructor(
+        public id: string,
+        public name: string,
+        public description: string,
+        public price: number,
+        public category: Category,
+    ) { }
+}
