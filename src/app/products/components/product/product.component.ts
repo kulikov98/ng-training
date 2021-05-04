06cb9212ac287251331e2ebdff8f4bbb4f6a342f
Vladimir Kulikov <vladimir_kulikov@epam.com>
2021-05-04 20:18:34 +0300

    make product component presentational

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { CartService } from 'src/app/cart/services/cart.service';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ProductModel } from '../../models/product.model';
 
 @Component({
@@ -7,16 +6,7 @@ import { ProductModel } from '../../models/product.model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
-  @Input() product!: ProductModel;
-
-  constructor(private cartService: CartService) { }
-
-  ngOnInit(): void {
-  }
-
-  onBuy(product: ProductModel): void {
-    console.log(`"${product.name}" have been added to the cart`);
-    this.cartService.addProduct(product);
-  }
+export class ProductComponent {
+  @Input() product: ProductModel | undefined;
+  @Output() addToCart = new EventEmitter<ProductModel>();
 }
