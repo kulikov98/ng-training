d285b6e58c05b09a9c2a3cf1f685740b2adc2418
Vladimir Kulikov <vladimir_kulikov@epam.com>
2021-05-04 20:18:07 +0300

    add isAvailable prop

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -8,10 +8,10 @@ import { Category } from '../enums/category.enum';
 export class ProductsService {
 
   private data: ProductModel[] = [
-    { id: '1', name: 'some name 1', description: 'some description 1', price: 1, category: Category['Accessories & Supplies'] },
-    { id: '2', name: 'some name 2', description: 'some description 2', price: 2, category: Category['Camera & Photo'] },
-    { id: '3', name: 'some name 3', description: 'some description 3', price: 3, category: Category['Car & Vehicle Electronics'] },
-    { id: '4', name: 'some name 4', description: 'some description 4', price: 4, category: Category['Cell Phones & Accessories'] },
+    { id: '1', name: 'some name 1', description: 'some description 1', price: 1, isAvailable: true, category: Category['Accessories & Supplies'] },
+    { id: '2', name: 'some name 2', description: 'some description 2', price: 2, isAvailable: true, category: Category['Camera & Photo'] },
+    { id: '3', name: 'some name 3', description: 'some description 3', price: 3, isAvailable: false, category: Category['Car & Vehicle Electronics'] },
+    { id: '4', name: 'some name 4', description: 'some description 4', price: 4, isAvailable: true, category: Category['Cell Phones & Accessories'] },
   ];
 
   constructor() { }
