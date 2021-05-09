d62622744fafa4cfeb5bc8ca32c1d1d15eeb99c6
Vladimir Kulikov <vladimir_kulikov@epam.com>
2021-05-09 15:36:17 +0300

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [
+        'src/**'
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        opacity: ['disabled']
+    },
+    plugins: [],
+}
