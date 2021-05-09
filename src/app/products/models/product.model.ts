bfcca7e935f6474682bbfcd2f18e5754f10b0ce2
Vladimir Kulikov <vladimir_kulikov@epam.com>
2021-05-09 15:37:13 +0300

    add imageUrl

--- src/app/products/models/product.model.ts
+++ src/app/products/models/product.model.ts
@@ -5,6 +5,7 @@ export class ProductModel {
         public id: string,
         public name: string,
         public description: string,
+        public imageUrl: string,
         public price: number,
         public isAvailable: boolean,
         public category: Category,
