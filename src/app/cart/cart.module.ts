a7a72a274cdcdbe70566703fb416ddae8775a967
Vladimir Kulikov <vladimir_kulikov@epam.com>
2021-06-21 11:19:18 +0300

    import common module only in shared module

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { CartListComponent } from './components/cart-list/cart-list.component';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
 import { SharedModule } from '../shared/shared.module';
@@ -12,7 +11,6 @@ import { SharedModule } from '../shared/shared.module';
     CartItemComponent
   ],
   imports: [
-    CommonModule,
     SharedModule,
   ],
   exports: [
